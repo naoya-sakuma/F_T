5dec16da2428e2303037221637046561c8cef828
naoyasakuma <4869ns@gmail.com>
2021-02-19 12:06:15 +0900

    [add]ルーティングを作成

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,9 @@
 class PlansController < ApplicationController
+  def day1
+  end
+
+  def day2
+  end
+
+  def day3
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "day1", to "plans#day1"
+  get "day2", to "plans#day2"
+  get "day3", to "plans#day3"
 end
